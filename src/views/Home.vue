dcda3118aaafc2978928751b843c762e7b389cf4
JackMarx <seriousfools@gmail.com>
2019-06-27 10:23:17 -0500

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form v-on:submit.prevent="submit()">
+      <div>
+        Name: <input v-model="newName">
+      </div>
+      <div>
+        Avatar: <input type="file" v-on:change="setFile($event)" ref="fileInput">
+      </div>
+      <input type="submit" value="Go">
+    </form>
+
+    <div v-for="profile in profiles">
+      <h2>{{ profile.name }}</h2>
+      <img v-bind:src="profile.avatar" alt="">
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+var axios = require('axios');
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      profiles: [],
+      newName: "",
+      image: ""
+    };
+  },
+  created: function() {
+    axios
+      .get("http://localhost:3000/profiles")
+      .then(response => {
+        this.profiles = response.data;
+      });
+  },
+  methods: {
+    setFile: function(event) {
+      if (event.target.files.length > 0) {
+        this.image = event.target.files[0];
+      }
+    },
+    submit: function() {
+      var params = new FormData();
+      params.append("name", this.newName);
+      params.append("avatar", this.image);
+
+      axios
+        .post("http://localhost:3000/profiles", params)
+        .then(response => {
+          this.newName = "";
+          this.image = "";
+          this.$refs.fileInput.value = "";
+          this.profiles = response.data;
+        });
+    }
   }
 }
 </script>
